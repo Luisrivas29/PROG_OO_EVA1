66a35b1d0b13bf767b1b0466f2b58fc3186abcc1
Luisrivas29 <143302710+Luisrivas29@users.noreply.github.com>
2024-02-06 18:26:52 -0600

    Add files via upload

---
+++ EVA1_1_CLASES_OBJETOS/src/eva1_1_clases_objetos/EVA1_1_CLASES_OBJETOS.java
@@ -0,0 +1,25 @@
+package eva1_1_clases_objetos;
+
+
+public class EVA1_1_CLASES_OBJETOS {
+
+   
+    public static void main(String[] args) {
+       Persona perso1;// declaro un identificador (variable) de tipo persona
+       //solo se declaro la variable, aun no se crea el objeto
+       //crear el objeto consiste en asignar memoria.
+       int valor;
+    }
+    
+}
+
+//CLASE PERSONA
+/*
+//NombreDeLaClase --> Nomeclatura CamelCase
+class NombreDeLaClase {cuerpo de la clase}
+*/
+class Persona{
+
+
+
+}
